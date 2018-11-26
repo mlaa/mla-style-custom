4c4f6714aa24c9bee477447cb28b1241e7d337a5
Joseff Betancourt <jbetancourt@mla.org>
2018-11-26 14:34:08 -0500

    first commit

---
+++ mla-style-custom.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     MLA Style Custom
+ * Plugin URI:      https://github.com/mlaa/mla-style-custom
+ * Description:     Miscellaneous actions & filters for Humanities Commons.
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     mla-style-custom
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mla_Style_Custom
+ */
+
+/**
+ * Twitter post plugin
+ */
+require_once trailingslashit( __DIR__ ) . 'includes/style-twitter-post-editor.php';
